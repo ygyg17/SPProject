46214043d6d73d3b81cac54ee3d2a1a3d3465a9e
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-04 11:49:16 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const SUPABASE_CONFIG = {
+  URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co',
+  ANON_KEY: 'sb_publishable_pT0S-ws6hJKRI7ptL7uhAg_A_GtFdFf',
+  TASKS_API_URL: '...',   // diisi nanti setelah deploy Edge Function
+  LOGIN_PAGE: 'login.html'
+};
