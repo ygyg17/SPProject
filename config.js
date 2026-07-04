1399b83337e511148eaa92bcd95e04138e01da2f
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-04 11:53:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SUPABASE_CONFIG = {
   URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co',
   ANON_KEY: 'sb_publishable_pT0S-ws6hJKRI7ptL7uhAg_A_GtFdFf',
-  TASKS_API_URL: '...',   // diisi nanti setelah deploy Edge Function
+  TASKS_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/tasks-api',  
   LOGIN_PAGE: 'login.html'
 };
