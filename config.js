17839d00e3961ab5b4e624e26580682e5fc59906
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-05 08:47:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
 const SUPABASE_CONFIG = {
   URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co',
   ANON_KEY: 'sb_publishable_pT0S-ws6hJKRI7ptL7uhAg_A_GtFdFf',
@@ -6,6 +5,8 @@ const SUPABASE_CONFIG = {
   DESIGN_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/design-request-api',
   LOGIN_PAGE: 'login.html'
 };
+
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
 
 const CONFIG = {
   ADMIN_GAS_WEB_APP_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/administration-api'
