5df05c1e26015d5b12d2f211085fc515f74fb0d8
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-05 08:21:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const SUPABASE_CONFIG = {
   URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co',
   ANON_KEY: 'sb_publishable_pT0S-ws6hJKRI7ptL7uhAg_A_GtFdFf',
   TASKS_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/tasks-api',
+  DESIGN_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/design-request-api',
   LOGIN_PAGE: 'login.html'
 };
 
